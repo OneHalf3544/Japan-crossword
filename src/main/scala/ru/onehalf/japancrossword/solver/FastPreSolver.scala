3792ec886003787428154e54092d3ab95c9e472d
OneHalf_3544 <onehalf354@gmail.com>
2013-06-02 11:02:45 +0400

    Бабочка 2

--- src/main/scala/ru/onehalf/japancrossword/solver/FastPreSolver.scala
+++ src/main/scala/ru/onehalf/japancrossword/solver/FastPreSolver.scala
@@ -2,6 +2,7 @@ package ru.onehalf.japancrossword.solver
 
 import ru.onehalf.japancrossword.model.Line
 import ru.onehalf.japancrossword.model.Cell._
+import ru.onehalf.japancrossword.model.Cell.Cell
 
 /**
  * <p/>
@@ -12,26 +13,20 @@ import ru.onehalf.japancrossword.model.Cell._
  */
 object FastPreSolver extends LineSolver {
 
+  val SEPARATOR = List(CLEARED)
+
   /**
    * Заполнить линию (Меняем значение только если оно еще не оперделено в модели)
    * @param metadata Данные по ожидаемому заполнению линии (цифры с краев кроссворда)
    * @param currentData Текущие данные
    */
   def fillLine(metadata: Array[Int], currentData: Line): List[Cell] = {
+    assert(metadata.nonEmpty)
 
     val length = metadata.sum + metadata.size - 1
     assert(currentData.size >= length, "wrong length: " + length)
 
-    if (metadata.isEmpty) {
-      return List.fill(currentData.size)(CLEARED)
-    }
-
-    val separator = List(CLEARED)
-
-    // Последовательность, которую будем двигать от края до края
-    var cellSequence = List.fill(metadata.head)(FILLED)
-    if (metadata.size > 1)
-      cellSequence = cellSequence ::: metadata.tail.map(s => separator ::: List.fill(s)(FILLED)).reduce((a, b) => a ::: b)
+    val cellSequence: List[Cell] = createSequence(metadata)
 
     var variants: List[List[Cell]] = List()
     for (i <- 0 to currentData.size - length) {
@@ -39,5 +34,18 @@ object FastPreSolver extends LineSolver {
     }
 
     variants.reduce(reduceLines)
+  }
+
+  /**
+   * @return Последовательность, которую будем двигать от края до края
+   */
+  def createSequence(metadata: Array[Int]): List[Cell] = {
+    val cellSequence = List.fill(metadata.head)(FILLED)
+
+    if (metadata.size <= 1) {
+      return cellSequence
+    }
+
+    cellSequence ::: metadata.tail.map(s => SEPARATOR ::: List.fill(s)(FILLED)).reduce((a, b) => a ::: b)
   }
 }
